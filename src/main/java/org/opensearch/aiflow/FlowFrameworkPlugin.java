e2310dde038e868b491a3dbb9da7dad1d39169c8
Daniel Widdis <widdis@gmail.com>
2023-08-15 17:54:05 -0700

    Initialize source code with opensearch-plugin-template-java
    Signed-off-by: Daniel Widdis <widdis@gmail.com>

---
+++ src/main/java/org/opensearch/aiflow/FlowFrameworkPlugin.java
@@ -0,0 +1,14 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+package org.opensearch.aiflow;
+
+import org.opensearch.plugins.Plugin;
+
+public class FlowFrameworkPlugin extends Plugin {
+    // Implement the relevant Plugin Interfaces here
+}
