b8869ef10c1de0204ce46935b3b7f945655a6186
Owais Kazi <owaiskazi19@gmail.com>
2023-09-13 15:50:49 -0700

    Adds CreateIndex building block
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>

--- src/main/java/org/opensearch/flowframework/workflow/CreateIndexStep.java
+++ src/main/java/org/opensearch/flowframework/workflow/CreateIndexStep.java
@@ -10,26 +10,54 @@ package org.opensearch.flowframework.workflow;
 
 import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.opensearch.action.ActionListener;
 import org.opensearch.action.admin.indices.create.CreateIndexRequest;
-import org.opensearch.client.AdminClient;
+import org.opensearch.action.admin.indices.create.CreateIndexResponse;
+import org.opensearch.client.Client;
 import org.opensearch.common.xcontent.XContentType;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.concurrent.CompletableFuture;
 
-public class CreateIndexStep implements Workflow {
+public class CreateIndexStep implements WorkflowStep {
 
-    AdminClient adminClient;
+    private static final Logger logger = LogManager.getLogger(CreateIndexStep.class);
+    private Client client;
+    private final String CREATE_INDEX_STEP = "create_index_step";
+
+    public CreateIndexStep(Client client) {
+        this.client = client;
+    }
 
     @Override
-    public CompletableFuture<Workflow> execute() throws Exception {
+    public CompletableFuture<WorkflowData> execute(WorkflowData data) {
 
-        // ActionListener<CreateIndexResponse> actionListener
+        ActionListener<CreateIndexResponse> actionListener = new ActionListener<>() {
+
+            @Override
+            public void onResponse(CreateIndexResponse createIndexResponse) {
+                logger.info("created index:{}");
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                logger.error("Index creation failed", e);
+            }
+        };
+
+        // Fetch indexName, fileName and settings from WorkflowData
         CreateIndexRequest request = new CreateIndexRequest(indexName).mapping(getIndexMappings(fileName), XContentType.JSON)
             .settings(settings);
-        adminClient.indices().create(request, actionListener);
+        client.admin().indices().create(request, actionListener);
+        return null;
+    }
 
+    @Override
+    public String getName() {
+        return CREATE_INDEX_STEP;
     }
 
     /**
