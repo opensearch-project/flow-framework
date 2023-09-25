a97b7d0f1f1cc48cb12420c3de8412e50f0de584
Owais Kazi <owaiskazi19@gmail.com>
2023-09-25 10:44:33 -0700

    Adds Create Index Building block  (#38)
    * Initial impelmentation of CreateIndex
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Adds CreateIndex building block
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Integrated WorkflowData and made the request async
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Addressed PR comments
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Added unit test and type field for fetching the payload
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Addressed PR Comments
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    ---------
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>

--- src/main/java/org/opensearch/flowframework/FlowFrameworkPlugin.java
+++ src/main/java/org/opensearch/flowframework/FlowFrameworkPlugin.java
@@ -8,11 +8,47 @@
  */
 package org.opensearch.flowframework;
 
+import com.google.common.collect.ImmutableList;
+import org.opensearch.client.Client;
+import org.opensearch.cluster.metadata.IndexNameExpressionResolver;
+import org.opensearch.cluster.service.ClusterService;
+import org.opensearch.core.common.io.stream.NamedWriteableRegistry;
+import org.opensearch.core.xcontent.NamedXContentRegistry;
+import org.opensearch.env.Environment;
+import org.opensearch.env.NodeEnvironment;
+import org.opensearch.flowframework.workflow.CreateIndex.CreateIndexStep;
 import org.opensearch.plugins.Plugin;
+import org.opensearch.repositories.RepositoriesService;
+import org.opensearch.script.ScriptService;
+import org.opensearch.threadpool.ThreadPool;
+import org.opensearch.watcher.ResourceWatcherService;
+
+import java.util.Collection;
+import java.util.function.Supplier;
 
 /**
  * An OpenSearch plugin that enables builders to innovate AI apps on OpenSearch.
  */
 public class FlowFrameworkPlugin extends Plugin {
-    // Implement the relevant Plugin Interfaces here
+
+    private Client client;
+
+    @Override
+    public Collection<Object> createComponents(
+        Client client,
+        ClusterService clusterService,
+        ThreadPool threadPool,
+        ResourceWatcherService resourceWatcherService,
+        ScriptService scriptService,
+        NamedXContentRegistry xContentRegistry,
+        Environment environment,
+        NodeEnvironment nodeEnvironment,
+        NamedWriteableRegistry namedWriteableRegistry,
+        IndexNameExpressionResolver indexNameExpressionResolver,
+        Supplier<RepositoriesService> repositoriesServiceSupplier
+    ) {
+        this.client = client;
+        CreateIndexStep createIndexStep = new CreateIndexStep(client);
+        return ImmutableList.of(createIndexStep);
+    }
 }
