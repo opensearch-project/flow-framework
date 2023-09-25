718704da0e48072fe27aae763bc5cdbce15c130b
Daniel Widdis <widdis@gmail.com>
2023-09-25 08:13:17 -0700

    Change WorkflowData from interface to class
    Signed-off-by: Daniel Widdis <widdis@gmail.com>

--- src/main/java/org/opensearch/flowframework/workflow/WorkflowData.java
+++ src/main/java/org/opensearch/flowframework/workflow/WorkflowData.java
@@ -12,29 +12,53 @@ import java.util.Collections;
 import java.util.Map;
 
 /**
- * Interface representing data provided as input to, and produced as output from, {@link WorkflowStep}s.
+ * Class encapsulating data provided as input to, and produced as output from, {@link WorkflowStep}s.
  */
-public interface WorkflowData {
+public class WorkflowData {
 
     /**
      * An object representing no data, useful when a workflow step has no required input or output.
      */
-    WorkflowData EMPTY = new WorkflowData() {
+    public static WorkflowData EMPTY = new WorkflowData();
+
+    private final Map<String, Object> content;
+    private final Map<String, String> params;
+
+    private WorkflowData() {
+        this(Collections.emptyMap(), Collections.emptyMap());
+    }
+
+    /**
+     * Instantiate this object with content and empty params.
+     * @param content The content map
+     */
+    public WorkflowData(Map<String, Object> content) {
+        this(content, Collections.emptyMap());
+    }
+
+    /**
+     * Instantiate this object with content and params.
+     * @param content The content map
+     * @param params The params map
+     */
+    public WorkflowData(Map<String, Object> content, Map<String, String> params) {
+        this.content = Map.copyOf(content);
+        this.params = Map.copyOf(params);
+    }
+
+    /**
+     * Returns a map which represents the content associated with a Rest API request or response.
+     * @return the content of this data.
+     */
+    public Map<String, Object> getContent() {
+        return this.content;
     };
 
     /**
-     * Accesses a map containing the content of the workflow step. This represents the data associated with a Rest API request.
-     * @return the content of this step.
+     * Returns a map represents the params associated with a Rest API request, parsed from the URI.
+     * @return the params of this data.
      */
-    default Map<String, Object> getContent() {
-        return Collections.emptyMap();
-    };
-
-    /**
-     * Accesses a map containing the params of this workflow step. This represents the params associated with a Rest API request, parsed from the URI.
-     * @return the params of this step.
-     */
-    default Map<String, String> getParams() {
-        return Collections.emptyMap();
+    public Map<String, String> getParams() {
+        return this.params;
     };
 }
