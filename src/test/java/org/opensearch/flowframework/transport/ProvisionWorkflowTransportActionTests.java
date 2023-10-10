b945bdd1a744d9b4fdd176f61f49fcd86c67d8dd
Joshua Palis <jpalis@amazon.com>
2023-10-10 18:34:34 +0000

    Add success test case for ProvisionWorkflowTransportAction
    Signed-off-by: Joshua Palis <jpalis@amazon.com>

--- src/test/java/org/opensearch/flowframework/transport/ProvisionWorkflowTransportActionTests.java
+++ src/test/java/org/opensearch/flowframework/transport/ProvisionWorkflowTransportActionTests.java
@@ -8,20 +8,34 @@
  */
 package org.opensearch.flowframework.transport;
 
+import org.opensearch.Version;
 import org.opensearch.action.get.GetResponse;
 import org.opensearch.action.support.ActionFilters;
 import org.opensearch.client.Client;
 import org.opensearch.common.settings.Settings;
 import org.opensearch.common.util.concurrent.ThreadContext;
+import org.opensearch.common.xcontent.XContentFactory;
 import org.opensearch.core.action.ActionListener;
+import org.opensearch.core.common.bytes.BytesReference;
+import org.opensearch.core.xcontent.ToXContent;
+import org.opensearch.core.xcontent.XContentBuilder;
+import org.opensearch.flowframework.model.Template;
+import org.opensearch.flowframework.model.Workflow;
+import org.opensearch.flowframework.model.WorkflowEdge;
+import org.opensearch.flowframework.model.WorkflowNode;
 import org.opensearch.flowframework.workflow.WorkflowProcessSorter;
+import org.opensearch.index.get.GetResult;
 import org.opensearch.tasks.Task;
 import org.opensearch.test.OpenSearchTestCase;
 import org.opensearch.threadpool.ThreadPool;
 import org.opensearch.transport.TransportService;
 
+import java.util.List;
+import java.util.Map;
+
 import org.mockito.ArgumentCaptor;
 
+import static org.opensearch.flowframework.common.CommonValue.GLOBAL_CONTEXT_INDEX;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
@@ -35,6 +49,7 @@ public class ProvisionWorkflowTransportActionTests extends OpenSearchTestCase {
     private Client client;
     private WorkflowProcessSorter workflowProcessSorter;
     private ProvisionWorkflowTransportAction provisionWorkflowTransportAction;
+    private Template template;
 
     @Override
     public void setUp() throws Exception {
@@ -51,6 +66,29 @@ public class ProvisionWorkflowTransportActionTests extends OpenSearchTestCase {
             workflowProcessSorter
         );
 
+        List<String> operations = List.of("operation");
+        Version templateVersion = Version.fromString("1.0.0");
+        List<Version> compatibilityVersions = List.of(Version.fromString("2.0.0"), Version.fromString("3.0.0"));
+        WorkflowNode nodeA = new WorkflowNode("A", "a-type", Map.of("foo", "bar"));
+        WorkflowNode nodeB = new WorkflowNode("B", "b-type", Map.of("baz", "qux"));
+        WorkflowEdge edgeAB = new WorkflowEdge("A", "B");
+        List<WorkflowNode> nodes = List.of(nodeA, nodeB);
+        List<WorkflowEdge> edges = List.of(edgeAB);
+        Workflow workflow = new Workflow(Map.of("key", "value"), nodes, edges);
+
+        this.template = new Template(
+            "test",
+            "description",
+            "use case",
+            operations,
+            templateVersion,
+            compatibilityVersions,
+            Map.ofEntries(Map.entry("userKey", "userValue"), Map.entry("userMapKey", Map.of("nestedKey", "nestedValue"))),
+            Map.of("provision", workflow),
+            Map.of("outputKey", "outputValue"),
+            Map.of("resourceKey", "resourceValue")
+        );
+
         ThreadPool clientThreadPool = mock(ThreadPool.class);
         ThreadContext threadContext = new ThreadContext(Settings.EMPTY);
 
@@ -59,7 +97,26 @@ public class ProvisionWorkflowTransportActionTests extends OpenSearchTestCase {
     }
 
     public void testProvisionWorkflow() {
-        // TODO : Add tests for success case
+
+        String workflowId = "1";
+        ActionListener<WorkflowResponse> listener = mock(ActionListener.class);
+        WorkflowRequest workflowRequest = new WorkflowRequest(workflowId, null);
+
+        doAnswer(invocation -> {
+            ActionListener<GetResponse> responseListener = invocation.getArgument(1);
+
+            XContentBuilder builder = XContentFactory.jsonBuilder();
+            this.template.toXContent(builder, ToXContent.EMPTY_PARAMS);
+            BytesReference templateBytesRef = BytesReference.bytes(builder);
+            GetResult getResult = new GetResult(GLOBAL_CONTEXT_INDEX, workflowId, 1, 1, 1, true, templateBytesRef, null, null);
+            responseListener.onResponse(new GetResponse(getResult));
+            return null;
+        }).when(client).get(any(), any());
+
+        provisionWorkflowTransportAction.doExecute(mock(Task.class), workflowRequest, listener);
+        ArgumentCaptor<WorkflowResponse> responseCaptor = ArgumentCaptor.forClass(WorkflowResponse.class);
+        verify(listener, times(1)).onResponse(responseCaptor.capture());
+        assertEquals(workflowId, responseCaptor.getValue().getWorkflowId());
     }
 
     public void testFailedToRetrieveTemplateFromGlobalContext() {
