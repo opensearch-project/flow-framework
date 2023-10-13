ca232e541566ca06959abc383ee8b6392a3ee944
Owais Kazi <owaiskazi19@gmail.com>
2023-10-13 14:27:28 -0700

    Fixed testPluginInstalled failure (#92)
    (cherry picked from commit 1b856deadeccfe0b87e572d8cfbb6909da70d1c7)
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>

--- src/test/java/org/opensearch/flowframework/FlowFrameworkPluginIT.java
+++ src/test/java/org/opensearch/flowframework/FlowFrameworkPluginIT.java
@@ -8,33 +8,22 @@
  */
 package org.opensearch.flowframework;
 
-import com.carrotsearch.randomizedtesting.annotations.ThreadLeakScope;
-
 import org.apache.http.util.EntityUtils;
 import org.opensearch.client.Request;
 import org.opensearch.client.Response;
-import org.opensearch.plugins.Plugin;
-import org.opensearch.test.OpenSearchIntegTestCase;
+import org.opensearch.test.rest.OpenSearchRestTestCase;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.Collection;
-import java.util.Collections;
 
-@ThreadLeakScope(ThreadLeakScope.Scope.NONE)
-@OpenSearchIntegTestCase.ClusterScope(scope = OpenSearchIntegTestCase.Scope.SUITE)
-public class FlowFrameworkPluginIT extends OpenSearchIntegTestCase {
-
-    @Override
-    protected Collection<Class<? extends Plugin>> nodePlugins() {
-        return Collections.singletonList(FlowFrameworkPlugin.class);
-    }
+public class FlowFrameworkPluginIT extends OpenSearchRestTestCase {
 
     public void testPluginInstalled() throws IOException {
-        Response response = createRestClient().performRequest(new Request("GET", "/_cat/plugins"));
+        Response response = client().performRequest(new Request("GET", "/_cat/plugins"));
         String body = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
 
         logger.info("response body: {}", body);
-        assertTrue(body.contains("flowframework"));
+        assertNotNull(body);
+        assertTrue(body.contains("opensearch-flow-framework"));
     }
 }
