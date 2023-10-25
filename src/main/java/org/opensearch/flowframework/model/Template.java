5152ad31be2c0d5201cf4350846140065bd65e21
Joshua Palis <jpalis@amazon.com>
2023-10-25 20:02:18 +0000

    Simplifying Template format, removing operations, resources created, user outputs
    Signed-off-by: Joshua Palis <jpalis@amazon.com>

--- src/main/java/org/opensearch/flowframework/model/Template.java
+++ src/main/java/org/opensearch/flowframework/model/Template.java
@@ -25,7 +25,6 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 import static org.opensearch.core.xcontent.XContentParserUtils.ensureExpectedToken;
-import static org.opensearch.flowframework.common.TemplateUtil.parseStringToStringMap;
 
 /**
  * The Template is the central data structure which configures workflows. This object is used to parse JSON communicated via REST API.
@@ -38,8 +37,6 @@ public class Template implements ToXContentObject {
     public static final String DESCRIPTION_FIELD = "description";
     /** The template field name for template use case */
     public static final String USE_CASE_FIELD = "use_case";
-    /** The template field name for template operations */
-    public static final String OPERATIONS_FIELD = "operations";
     /** The template field name for template version information */
     public static final String VERSION_FIELD = "version";
     /** The template field name for template version */
@@ -48,20 +45,13 @@ public class Template implements ToXContentObject {
     public static final String COMPATIBILITY_FIELD = "compatibility";
     /** The template field name for template workflows */
     public static final String WORKFLOWS_FIELD = "workflows";
-    /** The template field name for template user outputs */
-    public static final String USER_OUTPUTS_FIELD = "user_outputs";
-    /** The template field name for template resources created */
-    public static final String RESOURCES_CREATED_FIELD = "resources_created";
 
     private final String name;
     private final String description;
     private final String useCase; // probably an ENUM actually
-    private final List<String> operations; // probably an ENUM actually
     private final Version templateVersion;
     private final List<Version> compatibilityVersion;
     private final Map<String, Workflow> workflows;
-    private final Map<String, Object> userOutputs;
-    private final Map<String, Object> resourcesCreated;
 
     /**
      * Instantiate the object representing a use case template
@@ -69,33 +59,24 @@ public class Template implements ToXContentObject {
      * @param name The template's name
      * @param description A description of the template's use case
      * @param useCase A string defining the internal use case type
-     * @param operations Expected operations of this template. Should match defined workflows.
      * @param templateVersion The version of this template
      * @param compatibilityVersion OpenSearch version compatibility of this template
      * @param workflows Workflow graph definitions corresponding to the defined operations.
-     * @param userOutputs A map of essential API responses for backend to use and lookup.
-     * @param resourcesCreated A map of all the resources created.
      */
     public Template(
         String name,
         String description,
         String useCase,
-        List<String> operations,
         Version templateVersion,
         List<Version> compatibilityVersion,
-        Map<String, Workflow> workflows,
-        Map<String, Object> userOutputs,
-        Map<String, Object> resourcesCreated
+        Map<String, Workflow> workflows
     ) {
         this.name = name;
         this.description = description;
         this.useCase = useCase;
-        this.operations = List.copyOf(operations);
         this.templateVersion = templateVersion;
         this.compatibilityVersion = List.copyOf(compatibilityVersion);
         this.workflows = Map.copyOf(workflows);
-        this.userOutputs = Map.copyOf(userOutputs);
-        this.resourcesCreated = Map.copyOf(resourcesCreated);
     }
 
     @Override
@@ -104,11 +85,6 @@ public class Template implements ToXContentObject {
         xContentBuilder.field(NAME_FIELD, this.name);
         xContentBuilder.field(DESCRIPTION_FIELD, this.description);
         xContentBuilder.field(USE_CASE_FIELD, this.useCase);
-        xContentBuilder.startArray(OPERATIONS_FIELD);
-        for (String op : this.operations) {
-            xContentBuilder.value(op);
-        }
-        xContentBuilder.endArray();
 
         if (this.templateVersion != null || !this.compatibilityVersion.isEmpty()) {
             xContentBuilder.startObject(VERSION_FIELD);
@@ -131,18 +107,6 @@ public class Template implements ToXContentObject {
         }
         xContentBuilder.endObject();
 
-        xContentBuilder.startObject(USER_OUTPUTS_FIELD);
-        for (Entry<String, Object> e : userOutputs.entrySet()) {
-            xContentBuilder.field(e.getKey(), e.getValue());
-        }
-        xContentBuilder.endObject();
-
-        xContentBuilder.startObject(RESOURCES_CREATED_FIELD);
-        for (Entry<String, Object> e : resourcesCreated.entrySet()) {
-            xContentBuilder.field(e.getKey(), e.getValue());
-        }
-        xContentBuilder.endObject();
-
         return xContentBuilder.endObject();
     }
 
@@ -157,12 +121,9 @@ public class Template implements ToXContentObject {
         String name = null;
         String description = "";
         String useCase = "";
-        List<String> operations = new ArrayList<>();
         Version templateVersion = null;
         List<Version> compatibilityVersion = new ArrayList<>();
         Map<String, Workflow> workflows = new HashMap<>();
-        Map<String, Object> userOutputs = new HashMap<>();
-        Map<String, Object> resourcesCreated = new HashMap<>();
 
         ensureExpectedToken(XContentParser.Token.START_OBJECT, parser.currentToken(), parser);
         while (parser.nextToken() != XContentParser.Token.END_OBJECT) {
@@ -177,12 +138,6 @@ public class Template implements ToXContentObject {
                     break;
                 case USE_CASE_FIELD:
                     useCase = parser.text();
-                    break;
-                case OPERATIONS_FIELD:
-                    ensureExpectedToken(XContentParser.Token.START_ARRAY, parser.currentToken(), parser);
-                    while (parser.nextToken() != XContentParser.Token.END_ARRAY) {
-                        operations.add(parser.text());
-                    }
                     break;
                 case VERSION_FIELD:
                     ensureExpectedToken(XContentParser.Token.START_OBJECT, parser.currentToken(), parser);
@@ -212,42 +167,6 @@ public class Template implements ToXContentObject {
                         workflows.put(workflowFieldName, Workflow.parse(parser));
                     }
                     break;
-                case USER_OUTPUTS_FIELD:
-                    ensureExpectedToken(XContentParser.Token.START_OBJECT, parser.currentToken(), parser);
-                    while (parser.nextToken() != XContentParser.Token.END_OBJECT) {
-                        String userOutputsFieldName = parser.currentName();
-                        switch (parser.nextToken()) {
-                            case VALUE_STRING:
-                                userOutputs.put(userOutputsFieldName, parser.text());
-                                break;
-                            case START_OBJECT:
-                                userOutputs.put(userOutputsFieldName, parseStringToStringMap(parser));
-                                break;
-                            default:
-                                throw new IOException("Unable to parse field [" + userOutputsFieldName + "] in a user_outputs object.");
-                        }
-                    }
-                    break;
-
-                case RESOURCES_CREATED_FIELD:
-                    ensureExpectedToken(XContentParser.Token.START_OBJECT, parser.currentToken(), parser);
-                    while (parser.nextToken() != XContentParser.Token.END_OBJECT) {
-                        String resourcesCreatedField = parser.currentName();
-                        switch (parser.nextToken()) {
-                            case VALUE_STRING:
-                                resourcesCreated.put(resourcesCreatedField, parser.text());
-                                break;
-                            case START_OBJECT:
-                                resourcesCreated.put(resourcesCreatedField, parseStringToStringMap(parser));
-                                break;
-                            default:
-                                throw new IOException(
-                                    "Unable to parse field [" + resourcesCreatedField + "] in a resources_created object."
-                                );
-                        }
-                    }
-                    break;
-
                 default:
                     throw new IOException("Unable to parse field [" + fieldName + "] in a template object.");
             }
@@ -256,17 +175,7 @@ public class Template implements ToXContentObject {
             throw new IOException("An template object requires a name.");
         }
 
-        return new Template(
-            name,
-            description,
-            useCase,
-            operations,
-            templateVersion,
-            compatibilityVersion,
-            workflows,
-            userOutputs,
-            resourcesCreated
-        );
+        return new Template(name, description, useCase, templateVersion, compatibilityVersion, workflows);
     }
 
     /**
@@ -339,14 +248,6 @@ public class Template implements ToXContentObject {
     }
 
     /**
-     * Operations this use case supports
-     * @return the operations
-     */
-    public List<String> operations() {
-        return operations;
-    }
-
-    /**
      * The version of this template
      * @return the templateVersion
      */
@@ -363,27 +264,11 @@ public class Template implements ToXContentObject {
     }
 
     /**
-     * Workflows encoded in this template, generally corresponding to the operations returned by {@link #operations()}.
+     * Workflows encoded in this template
      * @return the workflows
      */
     public Map<String, Workflow> workflows() {
         return workflows;
-    }
-
-    /**
-     * A map of essential API responses
-     * @return the userOutputs
-     */
-    public Map<String, Object> userOutputs() {
-        return userOutputs;
-    }
-
-    /**
-     * A map of all the resources created
-     * @return the resources created
-     */
-    public Map<String, Object> resourcesCreated() {
-        return resourcesCreated;
     }
 
     @Override
@@ -394,18 +279,12 @@ public class Template implements ToXContentObject {
             + description
             + ", useCase="
             + useCase
-            + ", operations="
-            + operations
             + ", templateVersion="
             + templateVersion
             + ", compatibilityVersion="
             + compatibilityVersion
             + ", workflows="
             + workflows
-            + ", userOutputs="
-            + userOutputs
-            + ", resourcesCreated="
-            + resourcesCreated
             + "]";
     }
 }
