8e85f98d13e05e08cbcedd9c681d628d347743a2
Daniel Widdis <widdis@gmail.com>
2023-10-31 11:34:27 -0700

    Fix Create Connector actions parsing (#127)
    * Fix Create Connector actions parsing
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    * Fix tests
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    * Handle exceptions
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    * Fix parameter key in tests
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    ---------
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>

--- src/test/java/org/opensearch/flowframework/workflow/CreateConnectorStepTests.java
+++ src/test/java/org/opensearch/flowframework/workflow/CreateConnectorStepTests.java
@@ -10,6 +10,7 @@ package org.opensearch.flowframework.workflow;
 
 import org.opensearch.core.action.ActionListener;
 import org.opensearch.core.rest.RestStatus;
+import org.opensearch.flowframework.common.CommonValue;
 import org.opensearch.flowframework.exception.FlowFrameworkException;
 import org.opensearch.ml.client.MachineLearningNodeClient;
 import org.opensearch.ml.common.connector.ConnectorAction;
@@ -27,7 +28,6 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.verify;
@@ -44,38 +44,30 @@ public class CreateConnectorStepTests extends OpenSearchTestCase {
 
         Map<String, String> params = Map.ofEntries(Map.entry("endpoint", "endpoint"), Map.entry("temp", "7"));
         Map<String, String> credentials = Map.ofEntries(Map.entry("key1", "value1"), Map.entry("key2", "value2"));
+        Map<?, ?>[] actions = new Map<?, ?>[] {
+            Map.ofEntries(
+                Map.entry(ConnectorAction.ACTION_TYPE_FIELD, ConnectorAction.ActionType.PREDICT.name()),
+                Map.entry(ConnectorAction.METHOD_FIELD, "post"),
+                Map.entry(ConnectorAction.URL_FIELD, "foo.test"),
+                Map.entry(
+                    ConnectorAction.REQUEST_BODY_FIELD,
+                    "{ \"model\": \"${parameters.model}\", \"messages\": ${parameters.messages} }"
+                )
+            ) };
 
         MockitoAnnotations.openMocks(this);
 
-        ConnectorAction.ActionType actionType = ConnectorAction.ActionType.PREDICT;
-        String method = "post";
-        String url = "foot.test";
-
         inputData = new WorkflowData(
             Map.ofEntries(
-                Map.entry("name", "test"),
-                Map.entry("description", "description"),
-                Map.entry("version", "1"),
-                Map.entry("protocol", "test"),
-                Map.entry("params", params),
-                Map.entry("credentials", credentials),
-                Map.entry(
-                    "actions",
-                    List.of(
-                        new ConnectorAction(
-                            actionType,
-                            method,
-                            url,
-                            null,
-                            "{ \"model\": \"${parameters.model}\", \"messages\": ${parameters.messages} }",
-                            null,
-                            null
-                        )
-                    )
-                )
+                Map.entry(CommonValue.NAME_FIELD, "test"),
+                Map.entry(CommonValue.DESCRIPTION_FIELD, "description"),
+                Map.entry(CommonValue.VERSION_FIELD, "1"),
+                Map.entry(CommonValue.PROTOCOL_FIELD, "test"),
+                Map.entry(CommonValue.PARAMETERS_FIELD, params),
+                Map.entry(CommonValue.CREDENTIALS_FIELD, credentials),
+                Map.entry(CommonValue.ACTIONS_FIELD, actions)
             )
         );
-
     }
 
     public void testCreateConnector() throws IOException, ExecutionException, InterruptedException {
@@ -83,6 +75,7 @@ public class CreateConnectorStepTests extends OpenSearchTestCase {
         String connectorId = "connect";
         CreateConnectorStep createConnectorStep = new CreateConnectorStep(machineLearningNodeClient);
 
+        @SuppressWarnings("unchecked")
         ArgumentCaptor<ActionListener<MLCreateConnectorResponse>> actionListenerCaptor = ArgumentCaptor.forClass(ActionListener.class);
 
         doAnswer(invocation -> {
@@ -104,6 +97,7 @@ public class CreateConnectorStepTests extends OpenSearchTestCase {
     public void testCreateConnectorFailure() throws IOException {
         CreateConnectorStep createConnectorStep = new CreateConnectorStep(machineLearningNodeClient);
 
+        @SuppressWarnings("unchecked")
         ArgumentCaptor<ActionListener<MLCreateConnectorResponse>> actionListenerCaptor = ArgumentCaptor.forClass(ActionListener.class);
 
         doAnswer(invocation -> {
