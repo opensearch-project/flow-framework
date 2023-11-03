aad17d0b07db30d9158f6c148a1c1b40df6bb3e3
Daniel Widdis <widdis@gmail.com>
2023-11-03 14:46:39 -0700

    Put plugin API behind a feature flag (#142)
    * Put plugin API behind a feature flag
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    * Add test for feature flag disabled
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    ---------
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>

--- src/test/java/org/opensearch/flowframework/FlowFrameworkPluginTests.java
+++ src/test/java/org/opensearch/flowframework/FlowFrameworkPluginTests.java
@@ -11,13 +11,21 @@ package org.opensearch.flowframework;
 import org.opensearch.client.AdminClient;
 import org.opensearch.client.Client;
 import org.opensearch.client.ClusterAdminClient;
+import org.opensearch.cluster.service.ClusterService;
+import org.opensearch.common.settings.ClusterSettings;
+import org.opensearch.common.settings.Setting;
 import org.opensearch.common.settings.Settings;
+import org.opensearch.env.Environment;
+import org.opensearch.flowframework.common.FlowFrameworkFeatureEnabledSetting;
 import org.opensearch.test.OpenSearchTestCase;
 import org.opensearch.threadpool.TestThreadPool;
 import org.opensearch.threadpool.ThreadPool;
 
 import java.io.IOException;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -31,6 +39,9 @@ public class FlowFrameworkPluginTests extends OpenSearchTestCase {
     private ClusterAdminClient clusterAdminClient;
     private ThreadPool threadPool;
     private Settings settings;
+    private Environment environment;
+    private ClusterSettings clusterSettings;
+    private ClusterService clusterService;
 
     @Override
     public void setUp() throws Exception {
@@ -41,7 +52,18 @@ public class FlowFrameworkPluginTests extends OpenSearchTestCase {
         when(client.admin()).thenReturn(adminClient);
         when(adminClient.cluster()).thenReturn(clusterAdminClient);
         threadPool = new TestThreadPool(FlowFrameworkPluginTests.class.getName());
-        settings = Settings.EMPTY;
+
+        environment = mock(Environment.class);
+        settings = Settings.builder().build();
+        when(environment.settings()).thenReturn(settings);
+
+        final Set<Setting<?>> settingsSet = Stream.concat(
+            ClusterSettings.BUILT_IN_CLUSTER_SETTINGS.stream(),
+            Stream.of(FlowFrameworkFeatureEnabledSetting.FLOW_FRAMEWORK_ENABLED)
+        ).collect(Collectors.toSet());
+        clusterSettings = new ClusterSettings(settings, settingsSet);
+        clusterService = mock(ClusterService.class);
+        when(clusterService.getClusterSettings()).thenReturn(clusterSettings);
     }
 
     @Override
@@ -52,10 +74,14 @@ public class FlowFrameworkPluginTests extends OpenSearchTestCase {
 
     public void testPlugin() throws IOException {
         try (FlowFrameworkPlugin ffp = new FlowFrameworkPlugin()) {
-            assertEquals(3, ffp.createComponents(client, null, threadPool, null, null, null, null, null, null, null, null).size());
+            assertEquals(
+                3,
+                ffp.createComponents(client, clusterService, threadPool, null, null, null, environment, null, null, null, null).size()
+            );
             assertEquals(2, ffp.getRestHandlers(null, null, null, null, null, null, null).size());
             assertEquals(2, ffp.getActions().size());
             assertEquals(1, ffp.getExecutorBuilders(settings).size());
+            assertEquals(1, ffp.getSettings().size());
         }
     }
 }
