5afa4bc3c33bcc37595f4ba61f25f8116630bd97
Owais Kazi <owaiskazi19@gmail.com>
2023-11-29 19:34:57 -0800

    [Feature/agent_framework] Registers a single agent with multiple tools (#198)
    * Initial register agent workflow step
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Added tools step
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Fixed ClassCastException
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Handled exception for Instant
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Added type Instant for WorklowNode Parser
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Removed created and last updated time
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Addressed parsing error
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Handled parsing of Long values for Instant
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Handled nested object for llm key
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Handled parsing error
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Another attempt to fix parsing error for llm
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Another attemp to fix XContent
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Fixed Parsing error
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Added tests for toolstep and javadocs
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Undo CI changes
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Addressing PR comments
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Addressing PR comments
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Handled interface changes
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Addressed conflicts
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Added TODO
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    ---------
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>

--- src/test/java/org/opensearch/flowframework/util/ParseUtilsTests.java
+++ src/test/java/org/opensearch/flowframework/util/ParseUtilsTests.java
@@ -12,9 +12,12 @@ import org.opensearch.common.xcontent.XContentFactory;
 import org.opensearch.core.xcontent.XContentBuilder;
 import org.opensearch.core.xcontent.XContentParser;
 import org.opensearch.test.OpenSearchTestCase;
+import org.junit.Assert;
 
 import java.io.IOException;
 import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ParseUtilsTests extends OpenSearchTestCase {
     public void testToInstant() throws IOException {
@@ -53,5 +56,21 @@ public class ParseUtilsTests extends OpenSearchTestCase {
         parser.nextToken();
         Instant instant = ParseUtils.parseInstant(parser);
         assertNull(instant);
+    }
+
+    public void testGetParameterMap() {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("key1", "value1");
+        parameters.put("key2", 2);
+        parameters.put("key3", 2.1);
+        parameters.put("key4", new int[] { 10, 20 });
+        parameters.put("key5", new Object[] { 1.01, "abc" });
+        Map<String, String> parameterMap = ParseUtils.getParameterMap(parameters);
+        Assert.assertEquals(5, parameterMap.size());
+        Assert.assertEquals("value1", parameterMap.get("key1"));
+        Assert.assertEquals("2", parameterMap.get("key2"));
+        Assert.assertEquals("2.1", parameterMap.get("key3"));
+        Assert.assertEquals("[10,20]", parameterMap.get("key4"));
+        Assert.assertEquals("[1.01,\"abc\"]", parameterMap.get("key5"));
     }
 }
