de8f15148172a87f33c2a8fa10612b1b6a60f90f
Daniel Widdis <widdis@gmail.com>
2023-12-01 10:34:10 -0800

    [Feature/agent_framework] Add Delete Connector Step (#211)
    * Add Delete Connector Step
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    * Add eclipse core runtime version resolution
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    * Use JDK17 for spotless
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    * Add Delete Connector Step
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    * Add eclipse core runtime version resolution
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    * Use JDK17 for spotless
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    * Fetch connector ID from appropriate previous node output
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    * Fix tests
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    * Test that actual ID is properly passed
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    * Update to current setup-java version
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    * Remove unneeded argument captors
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    ---------
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>

---
+++ src/test/java/org/opensearch/flowframework/workflow/DeleteConnectorStepTests.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+package org.opensearch.flowframework.workflow;
+
+import org.opensearch.action.delete.DeleteResponse;
+import org.opensearch.core.action.ActionListener;
+import org.opensearch.core.index.Index;
+import org.opensearch.core.index.shard.ShardId;
+import org.opensearch.core.rest.RestStatus;
+import org.opensearch.flowframework.common.CommonValue;
+import org.opensearch.flowframework.exception.FlowFrameworkException;
+import org.opensearch.ml.client.MachineLearningNodeClient;
+import org.opensearch.ml.common.transport.connector.MLCreateConnectorResponse;
+import org.opensearch.test.OpenSearchTestCase;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.verify;
+
+public class DeleteConnectorStepTests extends OpenSearchTestCase {
+    private WorkflowData inputData;
+
+    @Mock
+    MachineLearningNodeClient machineLearningNodeClient;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+
+        MockitoAnnotations.openMocks(this);
+
+        inputData = new WorkflowData(Map.of(CommonValue.CONNECTOR_ID, "test"), "test-id", "test-node-id");
+    }
+
+    public void testDeleteConnector() throws IOException, ExecutionException, InterruptedException {
+
+        String connectorId = randomAlphaOfLength(5);
+        DeleteConnectorStep deleteConnectorStep = new DeleteConnectorStep(machineLearningNodeClient);
+
+        doAnswer(invocation -> {
+            String connectorIdArg = invocation.getArgument(0);
+            ActionListener<DeleteResponse> actionListener = invocation.getArgument(1);
+            ShardId shardId = new ShardId(new Index("indexName", "uuid"), 1);
+            DeleteResponse output = new DeleteResponse(shardId, connectorIdArg, 1, 1, 1, true);
+            actionListener.onResponse(output);
+            return null;
+        }).when(machineLearningNodeClient).deleteConnector(any(String.class), any());
+
+        CompletableFuture<WorkflowData> future = deleteConnectorStep.execute(
+            inputData.getNodeId(),
+            inputData,
+            Map.of("step_1", new WorkflowData(Map.of("connector_id", connectorId), "workflowId", "nodeId")),
+            Map.of("step_1", "connector_id")
+        );
+        verify(machineLearningNodeClient).deleteConnector(any(String.class), any());
+
+        assertTrue(future.isDone());
+        assertEquals(connectorId, future.get().getContent().get("connector_id"));
+    }
+
+    public void testNoConnectorIdInOutput() throws IOException {
+        DeleteConnectorStep deleteConnectorStep = new DeleteConnectorStep(machineLearningNodeClient);
+
+        CompletableFuture<WorkflowData> future = deleteConnectorStep.execute(
+            inputData.getNodeId(),
+            inputData,
+            Collections.emptyMap(),
+            Collections.emptyMap()
+        );
+
+        assertTrue(future.isCompletedExceptionally());
+        ExecutionException ex = assertThrows(ExecutionException.class, () -> future.get().getContent());
+        assertTrue(ex.getCause() instanceof FlowFrameworkException);
+        assertEquals("Required field connector_id is not provided", ex.getCause().getMessage());
+    }
+
+    public void testDeleteConnectorFailure() throws IOException {
+        DeleteConnectorStep deleteConnectorStep = new DeleteConnectorStep(machineLearningNodeClient);
+
+        doAnswer(invocation -> {
+            ActionListener<MLCreateConnectorResponse> actionListener = invocation.getArgument(1);
+            actionListener.onFailure(new FlowFrameworkException("Failed to delete connector", RestStatus.INTERNAL_SERVER_ERROR));
+            return null;
+        }).when(machineLearningNodeClient).deleteConnector(any(String.class), any());
+
+        CompletableFuture<WorkflowData> future = deleteConnectorStep.execute(
+            inputData.getNodeId(),
+            inputData,
+            Map.of("step_1", new WorkflowData(Map.of("connector_id", "test"), "workflowId", "nodeId")),
+            Map.of("step_1", "connector_id")
+        );
+
+        verify(machineLearningNodeClient).deleteConnector(any(String.class), any());
+
+        assertTrue(future.isCompletedExceptionally());
+        ExecutionException ex = assertThrows(ExecutionException.class, () -> future.get().getContent());
+        assertTrue(ex.getCause() instanceof FlowFrameworkException);
+        assertEquals("Failed to delete connector", ex.getCause().getMessage());
+    }
+}
