7e6653190423b5aa17c28c578d5ab18b7d16ba0c
Joshua Palis <jpalis@amazon.com>
2023-12-05 00:05:50 +0000

    removing stray log
    Signed-off-by: Joshua Palis <jpalis@amazon.com>

--- src/main/java/org/opensearch/flowframework/transport/CreateWorkflowTransportAction.java
+++ src/main/java/org/opensearch/flowframework/transport/CreateWorkflowTransportAction.java
@@ -95,8 +95,6 @@ public class CreateWorkflowTransportAction extends HandledTransportAction<Workfl
             user
         );
 
-        logger.info("TESTING TEMPLATE : " + templateWithUser.toJson());
-
         if (request.isDryRun()) {
             try {
                 validateWorkflows(templateWithUser);
