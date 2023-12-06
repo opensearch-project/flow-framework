9b9c4067ffcbd333b7d43cccad08d7875a59c3e6
Daniel Widdis <widdis@gmail.com>
2023-12-05 21:08:44 -0800

    [Feature/agent_framework] Add Delete Agent Step (#246)
    Delete Agent Step
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>

---
+++ src/test/java/org/opensearch/flowframework/workflow/DeleteAgentStepTests.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+package org.opensearch.flowframework.workflow;
+
+import org.opensearch.action.delete.DeleteResponse;
+import org.opensearch.core.action.ActionListener;
+import org.opensearch.core.index.Index;
+import org.opensearch.core.index.shard.ShardId;
+import org.opensearch.core.rest.RestStatus;
+import org.opensearch.flowframework.exception.FlowFrameworkException;
+import org.opensearch.ml.client.MachineLearningNodeClient;
+import org.opensearch.test.OpenSearchTestCase;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.verify;
+
+public class DeleteAgentStepTests extends OpenSearchTestCase {
+    private WorkflowData inputData;
+
+    @Mock
+    MachineLearningNodeClient machineLearningNodeClient;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+
+        MockitoAnnotations.openMocks(this);
+
+        inputData = new WorkflowData(Collections.emptyMap(), "test-id", "test-node-id");
+    }
+
+    public void testDeleteAgent() throws IOException, ExecutionException, InterruptedException {
+
+        String agentId = randomAlphaOfLength(5);
+        DeleteAgentStep deleteAgentStep = new DeleteAgentStep(machineLearningNodeClient);
+
+        doAnswer(invocation -> {
+            String agentIdArg = invocation.getArgument(0);
+            ActionListener<DeleteResponse> actionListener = invocation.getArgument(1);
+            ShardId shardId = new ShardId(new Index("indexName", "uuid"), 1);
+            DeleteResponse output = new DeleteResponse(shardId, agentIdArg, 1, 1, 1, true);
+            actionListener.onResponse(output);
+            return null;
+        }).when(machineLearningNodeClient).deleteAgent(any(String.class), any());
+
+        CompletableFuture<WorkflowData> future = deleteAgentStep.execute(
+            inputData.getNodeId(),
+            inputData,
+            Map.of("step_1", new WorkflowData(Map.of("agent_id", agentId), "workflowId", "nodeId")),
+            Map.of("step_1", "agent_id")
+        );
+        verify(machineLearningNodeClient).deleteAgent(any(String.class), any());
+
+        assertTrue(future.isDone());
+        assertEquals(agentId, future.get().getContent().get("agent_id"));
+    }
+
+    public void testNoAgentIdInOutput() throws IOException {
+        DeleteAgentStep deleteAgentStep = new DeleteAgentStep(machineLearningNodeClient);
+
+        CompletableFuture<WorkflowData> future = deleteAgentStep.execute(
+            inputData.getNodeId(),
+            inputData,
+            Collections.emptyMap(),
+            Collections.emptyMap()
+        );
+
+        assertTrue(future.isCompletedExceptionally());
+        ExecutionException ex = assertThrows(ExecutionException.class, () -> future.get().getContent());
+        assertTrue(ex.getCause() instanceof FlowFrameworkException);
+        assertEquals("Missing required inputs [agent_id] in workflow [test-id] node [test-node-id]", ex.getCause().getMessage());
+    }
+
+    public void testDeleteAgentFailure() throws IOException {
+        DeleteAgentStep deleteAgentStep = new DeleteAgentStep(machineLearningNodeClient);
+
+        doAnswer(invocation -> {
+            ActionListener<DeleteResponse> actionListener = invocation.getArgument(1);
+            actionListener.onFailure(new FlowFrameworkException("Failed to delete agent", RestStatus.INTERNAL_SERVER_ERROR));
+            return null;
+        }).when(machineLearningNodeClient).deleteAgent(any(String.class), any());
+
+        CompletableFuture<WorkflowData> future = deleteAgentStep.execute(
+            inputData.getNodeId(),
+            inputData,
+            Map.of("step_1", new WorkflowData(Map.of("agent_id", "test"), "workflowId", "nodeId")),
+            Map.of("step_1", "agent_id")
+        );
+
+        verify(machineLearningNodeClient).deleteAgent(any(String.class), any());
+
+        assertTrue(future.isCompletedExceptionally());
+        ExecutionException ex = assertThrows(ExecutionException.class, () -> future.get().getContent());
+        assertTrue(ex.getCause() instanceof FlowFrameworkException);
+        assertEquals("Failed to delete agent", ex.getCause().getMessage());
+    }
+}
