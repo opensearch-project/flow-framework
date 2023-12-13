6447ac64db669e6ef93e24edd4d5b131802c85b8
Joshua Palis <jpalis@amazon.com>
2023-12-12 17:17:12 -0800

    [Feature/agent_framework] Add Get Workflow API to retrieve a stored template by workflow id (#273)
    * renaming status API implementation
    
    Signed-off-by: Joshua Palis <jpalis@amazon.com>
    
    * Adding GetWorkflow API
    
    Signed-off-by: Joshua Palis <jpalis@amazon.com>
    
    * addressing PR comments
    
    Signed-off-by: Joshua Palis <jpalis@amazon.com>
    
    * Adding todo reminder
    
    Signed-off-by: Joshua Palis <jpalis@amazon.com>
    
    ---------
    
    Signed-off-by: Joshua Palis <jpalis@amazon.com>

--- src/main/java/org/opensearch/flowframework/rest/RestGetWorkflowStateAction.java
+++ src/main/java/org/opensearch/flowframework/rest/RestGetWorkflowStateAction.java
@@ -19,8 +19,8 @@ import org.opensearch.core.xcontent.ToXContent;
 import org.opensearch.core.xcontent.XContentBuilder;
 import org.opensearch.flowframework.common.FlowFrameworkFeatureEnabledSetting;
 import org.opensearch.flowframework.exception.FlowFrameworkException;
-import org.opensearch.flowframework.transport.GetWorkflowAction;
-import org.opensearch.flowframework.transport.WorkflowRequest;
+import org.opensearch.flowframework.transport.GetWorkflowStateAction;
+import org.opensearch.flowframework.transport.GetWorkflowStateRequest;
 import org.opensearch.rest.BaseRestHandler;
 import org.opensearch.rest.BytesRestResponse;
 import org.opensearch.rest.RestRequest;
@@ -34,30 +34,25 @@ import static org.opensearch.flowframework.common.CommonValue.WORKFLOW_URI;
 import static org.opensearch.flowframework.common.FlowFrameworkSettings.FLOW_FRAMEWORK_ENABLED;
 
 /**
- * Rest Action to facilitate requests to get a stored template
+ * Rest Action to facilitate requests to get a workflow status
  */
-public class RestGetWorkflowAction extends BaseRestHandler {
+public class RestGetWorkflowStateAction extends BaseRestHandler {
 
-    private static final String GET_WORKFLOW_ACTION = "get_workflow";
-    private static final Logger logger = LogManager.getLogger(RestGetWorkflowAction.class);
+    private static final String GET_WORKFLOW_STATE_ACTION = "get_workflow_state";
+    private static final Logger logger = LogManager.getLogger(RestGetWorkflowStateAction.class);
     private FlowFrameworkFeatureEnabledSetting flowFrameworkFeatureEnabledSetting;
 
     /**
-     * Instantiates a new RestGetWorkflowAction
+     * Instantiates a new RestGetWorkflowStateAction
      * @param flowFrameworkFeatureEnabledSetting Whether this API is enabled
      */
-    public RestGetWorkflowAction(FlowFrameworkFeatureEnabledSetting flowFrameworkFeatureEnabledSetting) {
+    public RestGetWorkflowStateAction(FlowFrameworkFeatureEnabledSetting flowFrameworkFeatureEnabledSetting) {
         this.flowFrameworkFeatureEnabledSetting = flowFrameworkFeatureEnabledSetting;
     }
 
     @Override
     public String getName() {
-        return GET_WORKFLOW_ACTION;
-    }
-
-    @Override
-    public List<Route> routes() {
-        return ImmutableList.of(new Route(RestRequest.Method.GET, String.format(Locale.ROOT, "%s/{%s}", WORKFLOW_URI, WORKFLOW_ID)));
+        return GET_WORKFLOW_STATE_ACTION;
     }
 
     @Override
@@ -73,7 +68,7 @@ public class RestGetWorkflowAction extends BaseRestHandler {
 
             // Validate content
             if (request.hasContent()) {
-                throw new FlowFrameworkException("Invalid request format", RestStatus.BAD_REQUEST);
+                throw new FlowFrameworkException("No request body present", RestStatus.BAD_REQUEST);
             }
             // Validate params
             String workflowId = request.param(WORKFLOW_ID);
@@ -81,8 +76,9 @@ public class RestGetWorkflowAction extends BaseRestHandler {
                 throw new FlowFrameworkException("workflow_id cannot be null", RestStatus.BAD_REQUEST);
             }
 
-            WorkflowRequest workflowRequest = new WorkflowRequest(workflowId, null);
-            return channel -> client.execute(GetWorkflowAction.INSTANCE, workflowRequest, ActionListener.wrap(response -> {
+            boolean all = request.paramAsBoolean("all", false);
+            GetWorkflowStateRequest getWorkflowRequest = new GetWorkflowStateRequest(workflowId, all);
+            return channel -> client.execute(GetWorkflowStateAction.INSTANCE, getWorkflowRequest, ActionListener.wrap(response -> {
                 XContentBuilder builder = response.toXContent(channel.newBuilder(), ToXContent.EMPTY_PARAMS);
                 channel.sendResponse(new BytesRestResponse(RestStatus.OK, builder));
             }, exception -> {
@@ -92,7 +88,7 @@ public class RestGetWorkflowAction extends BaseRestHandler {
                     channel.sendResponse(new BytesRestResponse(ex.getRestStatus(), exceptionBuilder));
 
                 } catch (IOException e) {
-                    logger.error("Failed to send back get workflow exception", e);
+                    logger.error("Failed to send back provision workflow exception", e);
                     channel.sendResponse(new BytesRestResponse(ExceptionsHelper.status(e), e.getMessage()));
                 }
             }));
@@ -102,5 +98,12 @@ public class RestGetWorkflowAction extends BaseRestHandler {
                 new BytesRestResponse(ex.getRestStatus(), ex.toXContent(channel.newErrorBuilder(), ToXContent.EMPTY_PARAMS))
             );
         }
+    }
+
+    @Override
+    public List<Route> routes() {
+        return ImmutableList.of(
+            new Route(RestRequest.Method.GET, String.format(Locale.ROOT, "%s/{%s}/%s", WORKFLOW_URI, WORKFLOW_ID, "_status"))
+        );
     }
 }
