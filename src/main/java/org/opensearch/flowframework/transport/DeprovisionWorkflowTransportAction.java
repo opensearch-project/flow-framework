fc24526ef4baa7452a9d59684c377e7c42abe3ca
zane-neo <zaniu@amazon.com>
2024-01-17 16:18:42 +0800

    Fix hang in flow framework
    Signed-off-by: zane-neo <zaniu@amazon.com>

--- src/main/java/org/opensearch/flowframework/transport/DeprovisionWorkflowTransportAction.java
+++ src/main/java/org/opensearch/flowframework/transport/DeprovisionWorkflowTransportAction.java
@@ -46,6 +46,7 @@ import static org.opensearch.flowframework.common.CommonValue.PROVISIONING_PROGR
 import static org.opensearch.flowframework.common.CommonValue.PROVISION_START_TIME_FIELD;
 import static org.opensearch.flowframework.common.CommonValue.RESOURCES_CREATED_FIELD;
 import static org.opensearch.flowframework.common.CommonValue.STATE_FIELD;
+import static org.opensearch.flowframework.common.CommonValue.WORKFLOW_THREAD_POOL;
 import static org.opensearch.flowframework.common.WorkflowResources.getDeprovisionStepByWorkflowStep;
 import static org.opensearch.flowframework.common.WorkflowResources.getResourceByWorkflowStep;
 
@@ -101,7 +102,7 @@ public class DeprovisionWorkflowTransportAction extends HandledTransportAction<W
                 context.restore();
 
                 // Retrieve resources from workflow state and deprovision
-                executeDeprovisionSequence(workflowId, response.getWorkflowState().resourcesCreated(), listener);
+                threadPool.executor(WORKFLOW_THREAD_POOL).execute(() -> executeDeprovisionSequence(workflowId, response.getWorkflowState().resourcesCreated(), listener));
             }, exception -> {
                 String message = "Failed to get workflow state for workflow " + workflowId;
                 logger.error(message, exception);
