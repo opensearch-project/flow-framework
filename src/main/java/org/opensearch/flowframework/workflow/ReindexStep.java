180cd1e420704a311a9ec754acb869439b9f85f8
Owais Kazi <owaiskazi19@gmail.com>
2024-05-30 14:20:15 -0700

    Added workflow step for ReIndex Step (#718)
    * Initial commit for reindex workflow step with extra params
    
    Signed-off-by: owaiskazi19 <owaiskazi19@gmail.com>
    
    * Addressed PR comments
    
    Signed-off-by: owaiskazi19 <owaiskazi19@gmail.com>
    
    * Changed request per second to Float
    
    Signed-off-by: owaiskazi19 <owaiskazi19@gmail.com>
    
    * Addressed string array for source indices and removed state index entry
    
    Signed-off-by: owaiskazi19 <owaiskazi19@gmail.com>
    
    * Minor comments
    
    Signed-off-by: owaiskazi19 <owaiskazi19@gmail.com>
    
    ---------
    
    Signed-off-by: owaiskazi19 <owaiskazi19@gmail.com>
    Signed-off-by: martinpkr <devilmarti@abv.bg>

--- src/main/java/org/opensearch/flowframework/workflow/ReindexStep.java
+++ src/main/java/org/opensearch/flowframework/workflow/ReindexStep.java
@@ -13,6 +13,7 @@ import org.apache.logging.log4j.Logger;
 import org.opensearch.ExceptionsHelper;
 import org.opensearch.action.support.PlainActionFuture;
 import org.opensearch.client.Client;
+import org.opensearch.common.Booleans;
 import org.opensearch.core.action.ActionListener;
 import org.opensearch.core.common.Strings;
 import org.opensearch.core.rest.RestStatus;
@@ -90,9 +91,11 @@ public class ReindexStep implements WorkflowStep {
 
             String sourceIndices = (String) inputs.get(SOURCE_INDEX);
             String destinationIndex = (String) inputs.get(DESTINATION_INDEX);
-            Boolean refresh = ParseUtils.parseIfExists(inputs, REFRESH, Boolean.class);
-            Float requestsPerSecond = ParseUtils.parseIfExists(inputs, REQUESTS_PER_SECOND, Float.class);
-            Boolean requireAlias = ParseUtils.parseIfExists(inputs, REQUIRE_ALIAS, Boolean.class);
+            Boolean refresh = inputs.containsKey(REFRESH) ? Booleans.parseBoolean(inputs.get(REFRESH).toString()) : null;
+            Float requestsPerSecond = inputs.containsKey(REQUESTS_PER_SECOND)
+                ? Float.parseFloat(inputs.get(REQUESTS_PER_SECOND).toString())
+                : null;
+            Boolean requireAlias = inputs.containsKey(REQUIRE_ALIAS) ? Booleans.parseBoolean(inputs.get(REQUIRE_ALIAS).toString()) : null;
             Integer slices = (Integer) inputs.get(SLICES);
             Integer maxDocs = (Integer) inputs.get(MAX_DOCS);
 
