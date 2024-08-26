612c66d3182e1cb349e76a997dc5fa26ce0e5471
yuye-aws <yuyezhu@amazon.com>
2024-08-26 12:16:41 +0800

    implement unit test for connector, model and agent id
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>

--- src/test/java/org/opensearch/flowframework/workflow/ToolStepTests.java
+++ src/test/java/org/opensearch/flowframework/workflow/ToolStepTests.java
@@ -14,13 +14,23 @@ import org.opensearch.flowframework.exception.WorkflowStepException;
 import org.opensearch.ml.common.agent.MLToolSpec;
 import org.opensearch.test.OpenSearchTestCase;
 
-import java.io.IOException;
 import java.util.Collections;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
+import static org.opensearch.flowframework.common.WorkflowResources.AGENT_ID;
+import static org.opensearch.flowframework.common.WorkflowResources.CONNECTOR_ID;
+import static org.opensearch.flowframework.common.WorkflowResources.MODEL_ID;
+
 public class ToolStepTests extends OpenSearchTestCase {
     private WorkflowData inputData;
+    private WorkflowData inputDataWithConnectorId;
+    private WorkflowData inputDataWithModelId;
+    private WorkflowData inputDataWithAgentId;
+    private static final String mockedConnectorId = "mocked-connector-id";
+    private static final String mockedModelId = "mocked-model-id";
+    private static final String mockedAgentId = "mocked-agent-id";
+
     private WorkflowData boolStringInputData;
     private WorkflowData badBoolInputData;
 
@@ -39,6 +49,9 @@ public class ToolStepTests extends OpenSearchTestCase {
             "test-id",
             "test-node-id"
         );
+        inputDataWithConnectorId = new WorkflowData(Map.of(CONNECTOR_ID, mockedConnectorId), "test-id", "test-node-id");
+        inputDataWithModelId = new WorkflowData(Map.of(MODEL_ID, mockedModelId), "test-id", "test-node-id");
+        inputDataWithAgentId = new WorkflowData(Map.of(AGENT_ID, mockedAgentId), "test-id", "test-node-id");
         boolStringInputData = new WorkflowData(
             Map.ofEntries(
                 Map.entry("type", "type"),
@@ -63,7 +76,7 @@ public class ToolStepTests extends OpenSearchTestCase {
         );
     }
 
-    public void testTool() throws IOException, ExecutionException, InterruptedException {
+    public void testTool() throws ExecutionException, InterruptedException {
         ToolStep toolStep = new ToolStep();
 
         PlainActionFuture<WorkflowData> future = toolStep.execute(
@@ -88,7 +101,7 @@ public class ToolStepTests extends OpenSearchTestCase {
         assertEquals(MLToolSpec.class, future.get().getContent().get("tools").getClass());
     }
 
-    public void testBoolParseFail() throws IOException, ExecutionException, InterruptedException {
+    public void testBoolParseFail() {
         ToolStep toolStep = new ToolStep();
 
         PlainActionFuture<WorkflowData> future = toolStep.execute(
@@ -100,10 +113,64 @@ public class ToolStepTests extends OpenSearchTestCase {
         );
 
         assertTrue(future.isDone());
-        ExecutionException e = assertThrows(ExecutionException.class, () -> future.get());
+        ExecutionException e = assertThrows(ExecutionException.class, future::get);
         assertEquals(WorkflowStepException.class, e.getCause().getClass());
         WorkflowStepException w = (WorkflowStepException) e.getCause();
         assertEquals("Failed to parse value [yes] as only [true] or [false] are allowed.", w.getMessage());
         assertEquals(RestStatus.BAD_REQUEST, w.getRestStatus());
+    }
+
+    public void testToolWithConnectorId() throws ExecutionException, InterruptedException {
+        ToolStep toolStep = new ToolStep();
+
+        String createConnectorNodeName = "create_connector";
+        PlainActionFuture<WorkflowData> future = toolStep.execute(
+            inputData.getNodeId(),
+            inputData,
+            Map.of(createConnectorNodeName, inputDataWithConnectorId),
+            Map.of(createConnectorNodeName, CONNECTOR_ID),
+            Collections.emptyMap()
+        );
+        assertTrue(future.isDone());
+        Object tools = future.get().getContent().get("tools");
+        assertEquals(MLToolSpec.class, tools.getClass());
+        MLToolSpec mlToolSpec = (MLToolSpec) tools;
+        assertEquals(mlToolSpec.getParameters(), Map.of(CONNECTOR_ID, mockedConnectorId));
+    }
+
+    public void testToolWithModelId() throws ExecutionException, InterruptedException {
+        ToolStep toolStep = new ToolStep();
+
+        String createModelNodeName = "create_model";
+        PlainActionFuture<WorkflowData> future = toolStep.execute(
+            inputData.getNodeId(),
+            inputData,
+            Map.of(createModelNodeName, inputDataWithModelId),
+            Map.of(createModelNodeName, MODEL_ID),
+            Collections.emptyMap()
+        );
+        assertTrue(future.isDone());
+        Object tools = future.get().getContent().get("tools");
+        assertEquals(MLToolSpec.class, tools.getClass());
+        MLToolSpec mlToolSpec = (MLToolSpec) tools;
+        assertEquals(mlToolSpec.getParameters(), Map.of(MODEL_ID, mockedModelId));
+    }
+
+    public void testToolWithAgentId() throws ExecutionException, InterruptedException {
+        ToolStep toolStep = new ToolStep();
+
+        String createAgentNodeName = "create_agent";
+        PlainActionFuture<WorkflowData> future = toolStep.execute(
+            inputData.getNodeId(),
+            inputData,
+            Map.of(createAgentNodeName, inputDataWithAgentId),
+            Map.of(createAgentNodeName, AGENT_ID),
+            Collections.emptyMap()
+        );
+        assertTrue(future.isDone());
+        Object tools = future.get().getContent().get("tools");
+        assertEquals(MLToolSpec.class, tools.getClass());
+        MLToolSpec mlToolSpec = (MLToolSpec) tools;
+        assertEquals(mlToolSpec.getParameters(), Map.of(AGENT_ID, mockedAgentId));
     }
 }
