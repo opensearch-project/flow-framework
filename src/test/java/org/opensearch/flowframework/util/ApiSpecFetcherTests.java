65b5303fb51a23e2be02c0f0d6524f20826c749d
Junwei Dai <junweid@amazon.com>
2024-10-04 09:42:52 -0700

    remove duplication license
    Signed-off-by: Junwei Dai <junweid@amazon.com>

--- src/test/java/org/opensearch/flowframework/util/ApiSpecFetcherTests.java
+++ src/test/java/org/opensearch/flowframework/util/ApiSpecFetcherTests.java
@@ -8,14 +8,6 @@
  */
 package org.opensearch.flowframework.util;
 
-/*
- * SPDX-License-Identifier: Apache-2.0
- *
- * The OpenSearch Contributors require contributions made to
- * this file be licensed under the Apache-2.0 license or a
- * compatible open source license.
- */
-
 import org.opensearch.flowframework.exception.ApiSpecParseException;
 import org.opensearch.rest.RestRequest;
 import org.opensearch.test.OpenSearchTestCase;
