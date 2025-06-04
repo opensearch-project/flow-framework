bdfb3d0b9e69552d69954909672023e590e68dbb
Joshua Palis <jpalis@amazon.com>
2025-06-04 23:24:34 +0000

    Fixing imports
    Signed-off-by: Joshua Palis <jpalis@amazon.com>

--- src/main/java/org/opensearch/flowframework/workflow/RegisterAgentStep.java
+++ src/main/java/org/opensearch/flowframework/workflow/RegisterAgentStep.java
@@ -40,6 +40,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
